ddb7d69c2387028ee97816c1838268b5ed7fcb48
deepak bhoria <dpk22dev@gmail.com>
2018-12-06 14:07:27 +0530

    service added

--- src/main/java/com/example/atm/controller/AccountController.java
+++ src/main/java/com/example/atm/controller/AccountController.java
@@ -6,77 +6,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.atm.entity.AccountEntity;
-import com.example.atm.entity.AccountSummaryEntity;
-import com.example.atm.repository.AccountRepository;
-import com.example.atm.repository.AccountSummaryRepository;
+import com.example.atm.services.AccountService;
 
 @RestController
 @RequestMapping("/account")
 public class AccountController {
 	
 	@Autowired 
-	private AccountRepository accountRepository; 
-	
-	@Autowired
-	private AccountSummaryRepository accountSummaryRepository;
-	
-	@GetMapping("/amount")
-	public String check( @RequestParam(name="amount", required=true, defaultValue="") Long amount) {
-		AccountSummaryEntity ease = accountSummaryRepository.findByAmount(amount);
-		if( ease == null ) {
-        	return "account number doesn't exist in table";
-        }
-		return "ok";
-	}
+	private AccountService accountService;	
 	
 	@PostMapping("/deposit")
-    public synchronized String deposit(@RequestParam(name="accountNumber", required=true, defaultValue="") String accountNumber, 
+    public String deposit(@RequestParam(name="accountNumber", required=true, defaultValue="") String accountNumber, 
     		@RequestParam(name="amount", required=true, defaultValue="") Long amount) {
-        
-        AccountSummaryEntity ease = accountSummaryRepository.findByAccountNumber(accountNumber);
-        
-        if( ease == null ) {
-        	return "account number doesn't exist in table";
-        }
-        AccountEntity ae = new AccountEntity();
-        ae.setAccountNumber(accountNumber);
-        ae.setAmount(amount);
-        ae.setTransType('D');
-        accountRepository.save( ae );
-
-        Long curAmount = ease.getAmount();
-        ease.setAmount( curAmount + amount );
-        accountSummaryRepository.save( ease );
-                
-        return "balance udpated";
+		return accountService.deposit(accountNumber, amount);
+               
     }		
 	
 	@PostMapping("/withdraw")
-	public synchronized String withdraw(@RequestParam(name="accountNumber", required=true, defaultValue="") String accountNumber, 
+	public String withdraw(@RequestParam(name="accountNumber", required=true, defaultValue="") String accountNumber, 
     		@RequestParam(name="amount", required=true, defaultValue="") Long amount) {
-		
-        AccountSummaryEntity ease = accountSummaryRepository.findByAccountNumber(accountNumber);
-        if( ease == null ) {
-        	return "account number doesn't exist in table";
-        }
-        Long curAmount = ease.getAmount();
-        if( curAmount - amount > 0 ) {
-	        AccountEntity ae = new AccountEntity();
-	        ae.setAccountNumber(accountNumber);
-	        ae.setAmount(amount);
-	        ae.setTransType('W');
-	        accountRepository.save( ae );
-	        
-	        
-	        ease.setAmount( curAmount - amount );
-	        accountSummaryRepository.save( ease );
-	        return "amount updated";
-        } else {
-        	return "current balance is less!";
-        }
-        
+		return accountService.withdraw(accountNumber, amount);             
     }			
 	
 }
--- src/main/java/com/example/atm/services/AccountService.java
+++ src/main/java/com/example/atm/services/AccountService.java
@@ -1,5 +1,59 @@
 package com.example.atm.services;
 
-public class AccountService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.atm.entity.AccountEntity;
+import com.example.atm.entity.AccountSummaryEntity;
+import com.example.atm.repository.AccountRepository;
+import com.example.atm.repository.AccountSummaryRepository;
+
+@Service
+public class AccountService {
+	@Autowired 
+	private AccountRepository accountRepository; 
+	
+	@Autowired
+	private AccountSummaryRepository accountSummaryRepository;
+	
+	public synchronized String deposit( String accountNumber, Long amount ) {
+		AccountSummaryEntity ease = accountSummaryRepository.findByAccountNumber(accountNumber);
+
+		if (ease == null) {
+			return "account number doesn't exist in table";
+		}
+		AccountEntity ae = new AccountEntity();
+		ae.setAccountNumber(accountNumber);
+		ae.setAmount(amount);
+		ae.setTransType('D');
+		accountRepository.save(ae);
+
+		Long curAmount = ease.getAmount();
+		ease.setAmount(curAmount + amount);
+		accountSummaryRepository.save(ease);
+
+		return "balance udpated";
+	}
+	
+	public synchronized String withdraw( String accountNumber, Long amount  ) {
+		   AccountSummaryEntity ease = accountSummaryRepository.findByAccountNumber(accountNumber);
+	        if( ease == null ) {
+	        	return "account number doesn't exist in table";
+	        }
+	        Long curAmount = ease.getAmount();
+	        if( curAmount - amount > 0 ) {
+		        AccountEntity ae = new AccountEntity();
+		        ae.setAccountNumber(accountNumber);
+		        ae.setAmount(amount);
+		        ae.setTransType('W');
+		        accountRepository.save( ae );
+		        
+		        
+		        ease.setAmount( curAmount - amount );
+		        accountSummaryRepository.save( ease );
+		        return "amount updated";
+	        } else {
+	        	return "current balance is less!";
+	        }
+	}
 }
